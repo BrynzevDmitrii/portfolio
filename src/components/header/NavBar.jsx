3836b178cbdaac43a34774b257ac41526ed085d8
BrynzevDmitrii <Elf_happy@mail.ru>
2022-10-07 19:33:44 +0300

    NavMenu

---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>About Me</li>
+        <li>Contacts</li>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/page-fs/home/HomePage.jsx
@@ -0,0 +1,7 @@
+import { NavBar } from "../../components/header/NavBar";
+
+export default function HomePage() {
+    return(
+      <NavBar />
+    )
+}
